dc7eff121df9fcac38c4e94d193a7c1cf5bd7613
Archakula Usha <aaushasree@gmail.com>
2025-07-13 15:49:10 +0530

    Add files via upload

---
+++ proj01/app1/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+def home(request):
+    temp=loader.get_template('home.html')
+    return HttpResponse(temp.render())
+
+def master(request):
+    temp=loader.get_template('master.html')
+    return HttpResponse(temp.render())
